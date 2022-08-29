2aa8b6517c7400fd8e2ffacb0edece98024b6d66
Almamun7050 <105635038+Almamun7050@users.noreply.github.com>
2022-08-29 07:28:10 +0600

    some added

--- js/practice.js
+++
@@ -1,2 +0,0 @@
-const bondCode=` I am Fake James bond . My new code is: 00${7+1+2}`
-console.log(bondCode)
--- js/user.js
+++
@@ -1,9 +0,0 @@
-const loadUserFetch = () =>{
-    const url = `https://randomuser.me/api/?gender=female`
-    fetch(url)
-    .then(res => res.json())
-    .then(data =>  displayUser(data.results[0]))
-}
-const displayUser = user =>{
-    console.log(user)
-}
